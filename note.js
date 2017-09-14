d678b3635c405f8b55f7a3bb92c3874a7ee8d789
Аббас Гусенов <gusenov@live.ru>
2017-09-14 18:59:29 +0600

    0.0.0.2

--- note.js
+++ note.js
@@ -1,10 +1,15 @@
 (function () {
     'use strict';
     
+    // https://stackoverflow.com/a/9310752/2289640
+    function escapeRegExp(text) {
+        return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+    }
+    
     function setNote(notesStorage, tabUrl, noteText) {
         var nowDate = (new Date()).getTime(),
             page,
-            pageIdx = notesStorage.find("url", tabUrl);
+            pageIdx = notesStorage.find("url", escapeRegExp(tabUrl));
         
         if (pageIdx !== -1) {
             page = notesStorage.getRecordByIndex(pageIdx);
@@ -26,7 +31,7 @@
     function getNote(notesStorage, tabUrl) {
         var nowDate = (new Date()).getTime(),
             page,
-            pageIdx = notesStorage.find("url", tabUrl);
+            pageIdx = notesStorage.find("url", escapeRegExp(tabUrl));
         
         if (pageIdx !== -1) {
             page = notesStorage.getRecordByIndex(pageIdx);
@@ -45,18 +50,33 @@
         // Do nothing.
     }
     
+    function save(notesStorage, noteText) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (setNote(notesStorage, tabs[0].url, noteText)) {
+                chrome.pageAction.setIcon({ path: "icons.iconarchive.com/icons/fatcow/farm-fresh/16/note-edit-icon.png", tabId: tabs[0].id });
+            }
+        });
+    }
+    
     document.addEventListener('DOMContentLoaded', function () {
-        var notesStorage = new WebStore(localStorage, 'note', notesChangedCallback),
+        var port = chrome.runtime.connect(), // https://stackoverflow.com/q/39730493
+            notesStorage = new WebStore(localStorage, 'note', notesChangedCallback),
             notes = notesStorage.getAllRecords(),
             noteTextArea = document.getElementById("noteTextArea");
         
         noteTextArea.addEventListener("keyup", function () {
             var noteText = this.value;
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                if (setNote(notesStorage, tabs[0].url, noteText)) {
-                    chrome.pageAction.setIcon({ path: "icons.iconarchive.com/icons/fatcow/farm-fresh/16/note-edit-icon.png", tabId: tabs[0].id });
-                }
-            });      
+            save(notesStorage, noteText);
+        });
+        
+        noteTextArea.addEventListener("paste", function () {
+            var noteText = this.value;
+            save(notesStorage, noteText);
+        });
+        
+        noteTextArea.addEventListener("cut", function () {
+            var noteText = this.value;
+            save(notesStorage, noteText);
         });
         
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
